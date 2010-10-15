f3b29469f3d07ce3705f25b63f05496512f30e42
Seth Falcon <seth@userprimary.net>
2010-10-14 21:28:50 -0700

    initial

---
+++ nodes-schema.erl
@@ -0,0 +1,40 @@
+{
+  schema,
+  [
+   {version, "1.1"},
+   {default_field, "value"},
+   {default_op, "or"},
+   {n_val, 3},
+   {analyzer_factory, "com.basho.search.analysis.WhitespaceAnalyzerFactory"}
+  ],
+  [
+   {field, [
+            {name, "id"},
+            {type, string}
+           ]},
+   {dynamic_field, [
+                    {name, "*_text"},
+                    {type, string}
+                   ]},
+   {field, [
+            {name, "content"},
+            {type, string},
+            {analyzer_factory, "com.basho.search.analysis.WhitespaceAnalyzerFactory"}
+           ]},
+   {field, [
+            {name, "X_CHEF_id_CHEF_X"},
+            {type, string},
+            {analyzer_factory, "com.basho.search.analysis.WhitespaceAnalyzerFactory"}
+           ]},
+   {field, [
+            {name, "X_CHEF_database_CHEF_X"},
+            {type, string},
+            {analyzer_factory, "com.basho.search.analysis.WhitespaceAnalyzerFactory"}
+           ]},
+   {field, [
+            {name, "X_CHEF_type_CHEF_X"},
+            {type, string},
+            {analyzer_factory, "com.basho.search.analysis.WhitespaceAnalyzerFactory"}
+           ]}
+  ]
+ }.
